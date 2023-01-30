5f2f0fb19338e9ffc7b3252d03e3604b1d2eaffc
oscar cortes <oe.cortes@uniandes.edu.co>
2023-01-29 21:22:02 -0500

    Pruebas automatizadas para el login de la web

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "DermoApp",
+  e2e: {
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    baseUrl: 'http://localhost:8100',
+    //userAgent:'Mobile'
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
+  },
+});
